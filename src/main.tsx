f122ae3f11e5a768cc2c2f6694ed3e11fb51d201
Jomar Carlo <jcrorlanda@gmail.com>
2026-04-19 03:25:50 -0400

    Fix main.tsx import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../backup/App_backup_before_split.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
